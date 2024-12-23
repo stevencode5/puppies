4ffc8915cdbb3e98fae8143ba7390d9f62ede080
Steven Gonzalez <i_scardenas@admarketplace.com>
2024-12-23 16:08:24 -0500

    Create spring boot service with Java 17, Maven 3 and Spring Boot 3.6

---
+++ src/main/java/com/stevencode/puppies/PuppiesApplication.java
@@ -0,0 +1,13 @@
+package com.stevencode.puppies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stevencode/puppies/PuppiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevencode.puppies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
